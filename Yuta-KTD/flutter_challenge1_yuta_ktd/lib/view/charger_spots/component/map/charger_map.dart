2323d847a02a3d2b6c8f8fe287b9050e858c751c
Yuta-KTD <yuta.katada.2@gmail.com>
2023-09-06 08:34:49 +0900

    GoogleMapコンポーネント切り出し

---
+++ Yuta-KTD/flutter_challenge1_yuta_ktd/lib/view/charger_spots/component/map/charger_map.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+/// GoogleMap
+class ChargerMap extends ConsumerWidget {
+  final VoidCallback? onTap;
+  final void Function(GoogleMapController)? onMapCreated;
+  final Position? position;
+  const ChargerMap({
+    super.key,
+    this.onTap,
+    this.onMapCreated,
+    this.position,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return GoogleMap(
+      onTap: (_) => onTap,
+      mapType: MapType.normal,
+      myLocationEnabled: true,
+      initialCameraPosition: CameraPosition(
+        target: LatLng(
+          //positionが取得できない場合は初期座標を東京付近に設定する
+          position?.latitude ?? 36,
+          position?.longitude ?? 140,
+        ),
+        zoom: 16,
+      ),
+      onMapCreated: onMapCreated,
+      myLocationButtonEnabled: false,
+    );
+  }
+}

25539bedb1fcebb9904ec98b567c04d224eb8255
Yuta-KTD <yuta.katada.2@gmail.com>
2023-09-10 20:47:13 +0900

    検索ボタン配置と表示制御

--- Yuta-KTD/flutter_challenge1_yuta_ktd/lib/view/charger_spots/component/search_current_area_button.dart
+++ Yuta-KTD/flutter_challenge1_yuta_ktd/lib/view/charger_spots/component/search_current_area_button.dart
@@ -7,6 +7,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../../../constant/decolation_style.dart';
 import '../../../provider/charger_spots_async_provider.dart';
 import '../../../provider/map_controller_completer_provider.dart';
+import '../../../provider/show_search_button_provider.dart';
 
 /// 表示エリアでの検索ボタン
 class SearchCurrentAreaButton extends ConsumerWidget {
@@ -14,56 +15,59 @@ class SearchCurrentAreaButton extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final showSearchButtonNotifire = ref.watch(showSearchButtonProvider);
     final mapControllerCompleter = ref.watch(mapControllerCompleterProvider);
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Container(
-        decoration: BoxDecoration(
-          color: serchBoxBackgroundColor,
-          borderRadius: BorderRadius.circular(34.0),
-          boxShadow: const [
-            BoxShadow(
-              color: Color(0x26000000),
-              blurRadius: 29,
-              offset: Offset(0, 5),
-            ),
-          ],
-        ),
-        child: ElevatedButton(
-          onPressed: () => _onPressed(ref, mapControllerCompleter),
-          style: ElevatedButton.styleFrom(
-            backgroundColor: serchBoxBackgroundColor,
-            // disabledBackgroundColor: serchBoxBackgroundColor,
-            elevation: 0,
-            minimumSize: const Size(double.infinity, 62),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(34.0),
-              side: BorderSide.none, // 枠線をなくす
-            ),
-            padding: const EdgeInsets.all(16.24),
-          ),
-          child: const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(left: 27 - 16.24),
-                child: Text(
-                  'このエリアでスポットを検索',
-                  style: TextStyle(
-                    color: serchBoxContentColor,
-                    fontSize: 16,
+    return !showSearchButtonNotifire
+        ? const SizedBox.shrink()
+        : Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: serchBoxBackgroundColor,
+                borderRadius: BorderRadius.circular(34.0),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color(0x26000000),
+                    blurRadius: 29,
+                    offset: Offset(0, 5),
                   ),
+                ],
+              ),
+              child: ElevatedButton(
+                onPressed: () => _onPressed(ref, mapControllerCompleter),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: serchBoxBackgroundColor,
+                  // disabledBackgroundColor: serchBoxBackgroundColor,
+                  elevation: 0,
+                  minimumSize: const Size(double.infinity, 62),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(34.0),
+                    side: BorderSide.none, // 枠線をなくす
+                  ),
+                  padding: const EdgeInsets.all(16.24),
+                ),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 27 - 16.24),
+                      child: Text(
+                        'このエリアでスポットを検索',
+                        style: TextStyle(
+                          color: serchBoxContentColor,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    Icon(
+                      Icons.search,
+                      color: serchBoxContentColor,
+                    ),
+                  ],
                 ),
               ),
-              Icon(
-                Icons.search,
-                color: serchBoxContentColor,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 
   Future<void> _onPressed(WidgetRef ref,

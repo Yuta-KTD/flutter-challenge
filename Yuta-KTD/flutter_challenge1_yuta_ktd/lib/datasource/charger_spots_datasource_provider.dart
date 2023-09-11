a150e2968adabc61c0375cb25383a04b88d9908b
Yuta-KTD <yuta.katada.2@gmail.com>
2023-09-11 21:33:10 +0900

    ドキュメントに沿ったアーキテクチャに変更

--- Yuta-KTD/flutter_challenge1_yuta_ktd/lib/datasource/charger_spots_datasource_provider.dart
+++ Yuta-KTD/flutter_challenge1_yuta_ktd/lib/datasource/charger_spots_datasource_provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_challenge1_yuta_ktd/datasource/charger_spots_datasource.dart';
-import 'package:flutter_challenge1_yuta_ktd/api/dio_provider.dart';
+import 'package:flutter_challenge1_yuta_ktd/core/api/dio_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'charger_spots_datasource_interface.dart';
